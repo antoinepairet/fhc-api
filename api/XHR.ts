2a33e891c4cd3ea869e0bb92aa4d3cbc42035c0f
Antoine Pairet <antoine.pairet@gmail.com>
2019-11-23 04:42:46 +0100

    gen

--- api/XHR.ts
+++ api/XHR.ts
@@ -78,8 +78,8 @@ export namespace XHR {
       return (ct.startsWith("application/octet-stream")
         ? response.arrayBuffer()
         : ct.startsWith("application/json")
-        ? response.json()
-        : response.text()
+          ? response.json()
+          : response.text()
       ).then(d => new Data(response.status, ct, d))
     })
   }
